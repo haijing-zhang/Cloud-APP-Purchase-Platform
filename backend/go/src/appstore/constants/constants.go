1a8ef1492fc42429a245baa10cfb4880133b19d2
Haijing ZHANG <56798220+haijing-zhang@users.noreply.github.com>
2023-01-26 03:23:58 -0800

    Create constants.go

---
+++ backend/go/src/appstore/constants/constants.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    ES_URL = "http://34.125.107.216:9200"
+    ES_USERNAME = "ES_USERNAME"
+    ES_PASSWORD = "ES_PASSWORD"
+
+    STRIPE_API_KEY = "STRIP_SECRET_KEY"
+    GCS_BUCKET = "GCS_BUCKET"
+)
